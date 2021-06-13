9664886f3f178640fc20f018337113835dd8e4f2
prawat21 <puneetrawat852@gmail.com>
2021-06-13 21:55:01 +0530

    FirstCommit

--- src/ExceptionsHandlingConcepts/ThrowsException.java
+++ src/ExceptionsHandlingConcepts/ThrowsException.java
@@ -5,6 +5,11 @@ public class ThrowsException {
 	public static void main(String[] args) {
 		//String str = null;
 		//System.out.println(str.length()); // NullPointerException
+		
+		String str1 = "Puneet Rawat";
+		String str2 = new String("Puneet Raw");
+		System.out.println(str1==str2);//False
+		System.out.println(str1.compareTo(str2));// 
 
 	}
 
